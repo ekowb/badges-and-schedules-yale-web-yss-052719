69a2af40970f6b3b6aa12064aab43e00aaf34129
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 17:01:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  puts "Hello, my name is #{name}."
+end
 def assign_rooms(speakers)
   rooms = [*1..7]
   speakers.each do |person|
